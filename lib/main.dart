d4eb244dce7ba4b05791dc68efa95149fc6c8d7c
Bindu Khadka <bindu.khadka.np@gmail.com>
2023-07-01 17:53:15 +0545

    add widget to display job tags

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'api.dart';
+import 'job.dart';
 
 void main() async {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Job Portal',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
@@ -35,19 +36,38 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  late TextEditingController textEditingController;
-  late String searchItem;
+  List<Job> jobs = [];
+
   @override
   void initState() {
-    textEditingController = TextEditingController();
     super.initState();
+    fetchData();
   }
-  void onItemChanged(String text){
-    setState(() {
-      searchItem = text;
-    });
 
+  Future<void> fetchData() async {
+    try {
+      List<Job> data = await Api.getData();
+      setState(() {
+        jobs = data;
+      });
+      print("Data aayo hai data: $data");
+    } catch (error) {
+      print('Error fetching data: $error');
+    }
   }
+
+  List<String> jobTags(List<Job> jobs) {
+    Set<String> uniqueKeywords = Set<String>();
+
+    for (Job job in jobs) {
+      uniqueKeywords.addAll((job.keywords as List<dynamic>).cast<String>());
+    }
+    return uniqueKeywords.toList();
+  }
+
+  Set<String> selectedValues = {};
+  bool isTapped = false;
+  List<Map<String, dynamic>> filteredData = [];
 
   @override
   Widget build(BuildContext context) {
@@ -63,47 +83,78 @@ class _HomePageState extends State<HomePage> {
           children: [
             Container(
               padding: EdgeInsets.all(20),
-              height: 60,
-              child: TextField(
-                onSubmitted: (text) => onItemChanged(text),
-                decoration: InputDecoration(
-                    hintText: "Search", border: InputBorder.none),
+              height: 100,
+              child: Row(
+                children: selectedValues
+                    .map(
+                      (value) => Chip(
+                        onDeleted: () {
+                          setState(() {
+                            selectedValues.remove(value);
+                          });
+                        },
+                        label: Text(value),
+                      ),
+                    )
+                    .toList(),
               ),
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(8),
                 color: Colors.white,
               ),
             ),
+            const SizedBox(
+              height: 10,
+            ),
+            JobChip(
+              jobTags(jobs),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
             FutureBuilder(
-                future: Api.getData(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    return Expanded(
-                      child: ListView.builder(
-                          itemCount: snapshot.data.length,
-                          itemBuilder: (context, index) {
-                            return Container(
-                              padding: const EdgeInsets.all(8),
-                              margin: const EdgeInsets.all(10),
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                color: Colors.white,
-                              ),
-                              child: ListTile(
-                                  leading: CircleAvatar(
-                                    backgroundImage: NetworkImage(
-                                        snapshot.data[index].companyLogo),
-                                  ),
-                                  trailing: SizedBox(
-                                    width: 200,
-                                    child: ListView.builder(
-                                        itemCount: snapshot
-                                            .data[index].keywords.length,
-                                        scrollDirection: Axis.horizontal,
-                                        itemBuilder: (context, idx) {
-                                          return Row(
-                                            children: [
-                                              Container(
+              future: Api.getData(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Expanded(
+                    child: ListView.builder(
+                        itemCount: snapshot.data.length,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            padding: const EdgeInsets.all(8),
+                            margin: const EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.white,
+                            ),
+                            child: ListTile(
+                                leading: CircleAvatar(
+                                  backgroundImage: NetworkImage(
+                                      snapshot.data[index].companyLogo),
+                                ),
+                                trailing: SizedBox(
+                                  width: 200,
+                                  child: ListView.builder(
+                                      itemCount:
+                                          snapshot.data[index].keywords.length,
+                                      scrollDirection: Axis.horizontal,
+                                      itemBuilder: (context, idx) {
+                                        List<bool> selectedStates =
+                                            List.generate(snapshot.data.length,
+                                                (index) => false);
+                                        return Row(
+                                          children: [
+                                            GestureDetector(
+                                              onTap: () {
+                                                setState(() {
+                                                  selectedValues.add(snapshot
+                                                      .data[index]
+                                                      .keywords[idx]);
+                                                  selectedStates[idx] =
+                                                      !selectedStates[idx];
+                                                });
+                                              },
+                                              child: Container(
                                                 height: 30,
                                                 padding:
                                                     const EdgeInsets.symmetric(
@@ -114,65 +165,137 @@ class _HomePageState extends State<HomePage> {
                                                   snapshot.data[index]
                                                       .keywords[idx],
                                                   style: TextStyle(
-                                                      color: Color(0xff6A9EA0),
+                                                      color: selectedStates[idx]
+                                                          ? Colors.white
+                                                          : Color(0xff6A9EA0),
                                                       fontWeight:
                                                           FontWeight.bold),
                                                 ),
                                                 decoration: BoxDecoration(
                                                   borderRadius:
                                                       BorderRadius.circular(4),
-                                                  color:
-                                                      const Color(0xffebf3f3),
+                                                  color: selectedStates[idx]
+                                                      ? const Color(0xff6A9EA0)
+                                                      : const Color(0xffebf3f3),
                                                 ),
                                               ),
-                                              const SizedBox(
-                                                width: 5,
-                                              ),
-                                            ],
-                                          );
-                                        }),
-                                  ),
-                                  title: Text(snapshot.data[index].company),
-                                  subtitle: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        snapshot.data[index].position,
-                                        style: const TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            fontSize: 18),
-                                      ),
-                                      Row(
-                                        children: [
-                                          Text("1 day ago"),
-                                          SizedBox(
-                                            width: 20,
-                                          ),
-                                          Text(snapshot.data[index].timing),
-                                          SizedBox(
-                                            width: 20,
-                                          ),
-                                          Text(snapshot.data[index].location)
-                                        ],
-                                      ),
-                                    ],
-                                  )),
-                            );
-                          }),
-                    );
-                  } else if (snapshot.hasError) {
-                    print(snapshot.error);
-                    return const Center(
-                      child: Text("Could not display data"),
-                    );
-                  }
-                  return const CircularProgressIndicator();
-                }),
+                                            ),
+                                            const SizedBox(
+                                              width: 5,
+                                            ),
+                                          ],
+                                        );
+                                      }),
+                                ),
+                                title: Text(snapshot.data[index].company),
+                                subtitle: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      snapshot.data[index].position,
+                                      style: const TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18),
+                                    ),
+                                    Row(
+                                      children: [
+                                        Text("1 day ago"),
+                                        SizedBox(
+                                          width: 20,
+                                        ),
+                                        Text(snapshot.data[index].timing),
+                                        SizedBox(
+                                          width: 20,
+                                        ),
+                                        Text(snapshot.data[index].location)
+                                      ],
+                                    ),
+                                  ],
+                                )),
+                          );
+                        }),
+                  );
+                } else if (snapshot.hasError) {
+                  print(snapshot.error);
+                  return const Center(
+                    child: Text("Could not display data"),
+                  );
+                }
+                return const CircularProgressIndicator();
+              },
+            ),
+
+
           ],
         ),
       ),
     );
   }
+}
 
+class JobChip extends StatefulWidget {
+  final List<String> jobTags;
+
+  JobChip(this.jobTags);
+
+  @override
+  State<JobChip> createState() => _JobChipState();
+}
+
+class _JobChipState extends State<JobChip> {
+  late List<String> tags;
+  late List<bool> isSelected;
+
+  _JobChipState();
+
+  @override
+  void initState() {
+    super.initState();
+    tags = widget.jobTags;
+
+    isSelected = List<bool>.filled(20, false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      height: 100,
+      color: Colors.white,
+      child: ListView.builder(
+        itemCount: widget.jobTags.length,
+        scrollDirection: Axis.horizontal,
+        shrinkWrap: true,
+        itemBuilder: (context, index) {
+          return Row(
+            children: [
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    isSelected[index] = !isSelected[index];
+                    print(isSelected[index]);
+                  });
+                },
+                child: Chip(
+                  backgroundColor: isSelected[index]
+                      ? Color(0xff6A9EA0)
+                      : Colors.transparent,
+                  label: Text(
+                    widget.jobTags[index],
+                    style: TextStyle(
+                      color:
+                          isSelected[index] ? Colors.white : Color(0xff6A9EA0),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 8,
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
 }
